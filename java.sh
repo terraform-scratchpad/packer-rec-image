33f6b973a2b48a0a53157987de16889782cea85c
Abderrazak BOUADMA <bouadma.abderrazak@gmail.com>
2018-08-27 15:04:02 +0200

    added groovy installation

--- java.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get update
-sudo apt-get -y install software-properties-common
-sudo add-apt-repository "deb http://ppa.launchpad.net/webupd8team/java/ubuntu xenial main"
-sudo apt-get update
-sudo apt-get -y install oracle-java8-installer
